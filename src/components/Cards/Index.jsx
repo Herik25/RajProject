bed2a1763176e8fa56caca5eb9de4370cf877e1c
Herik25 <128887958+Herik25@users.noreply.github.com>
2024-08-03 10:57:05 +0530

    cards commit

--- src/components/Cards/Index.jsx
+++ src/components/Cards/Index.jsx
@@ -1,24 +1,54 @@
-import React from 'react'
+import React from "react";
 
 function Cards() {
   return (
-    <div className=' px-32'>
-        <div className=' flex justify-between gap-10 border-gray-100 rounded-lg border-2 w-[300px]'>
-            <div>
-                <div className=' h-[250px] w-full bg-gray-100'>
-                    
-                </div>
-                {/* <div>
-                    <h1>The future of web 
-                    design, today</h1>
-                    <p>The future of web design, today</p>
-                    <button>asdf</button>
-                </div> */}
-
-            </div>
+    <div className=" px-32">
+      <div className=" flex justify-between items-center gap-10  rounded-lg">
+        <div className="border-gray-100 border-2">
+          <div className=" h-[250px]  bg-gray-100"></div>
+          <div className=" flex flex-col justify-between min-h-[300px] p-6 w-full">
+            <h1 className=" text-4xl text-center">
+              The future of web design, today
+            </h1>
+            <p className=" text-lg text-center">
+              The future of web design, today
+            </p>
+            <button className=" border-gray-100 border-2 p-5 rounded-md text-[#000] font-semibold">
+              Call to action
+            </button>
+          </div>
         </div>
+        <div className="border-gray-100 border-2">
+          <div className=" h-[250px]  bg-gray-100"></div>
+          <div className=" flex flex-col justify-between min-h-[300px] p-6 w-full">
+            <h1 className=" text-4xl text-center">
+              The future of web design, today
+            </h1>
+            <p className=" text-lg text-center">
+              The future of web design, today
+            </p>
+            <button className=" border-gray-100 border-2 p-5 rounded-md text-[#000] font-semibold">
+              Call to action
+            </button>
+          </div>
+        </div>
+        <div className="border-gray-100 border-2">
+          <div className=" h-[250px]  bg-gray-100"></div>
+          <div className=" flex flex-col justify-between min-h-[300px] p-6 w-full">
+            <h1 className=" text-4xl text-center">
+              The future of web design, today
+            </h1>
+            <p className=" text-lg text-center">
+              The future of web design, today
+            </p>
+            <button className=" border-gray-100 border-2 p-5 rounded-md text-[#000] font-semibold">
+              Call to action
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
