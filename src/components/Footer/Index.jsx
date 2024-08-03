13f097ee995d30815b6b3891bdf131b14f4efa25
Herik25 <128887958+Herik25@users.noreply.github.com>
2024-08-03 11:19:45 +0530

    footer 1

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -16,13 +16,46 @@ function Footer() {
         </div>
       </div>
       <div className=" flex flex-col gap-5">
-        <h5 className=" text-white">More information</h5>
-        <div>
-          <div className=" h-5 w-5 rounded-md bg-gray-500"></div>
+        <h5 className=" text-white text-2xl">More information</h5>
+        <div className=" flex gap-4">
+          <div className=" h-5 w-5 rounded-md bg-gray-500"></div>{" "}
+          <span>Home</span>
         </div>
-        <div>stay in contact</div>
+        <div className=" flex gap-4">
+          <div className=" h-5 w-5 rounded-md bg-gray-500"></div>{" "}
+          <span>About us</span>
+        </div>
+        <div className=" flex gap-4">
+          <div className=" h-5 w-5 rounded-md bg-gray-500"></div>{" "}
+          <span>Shop</span>
+        </div>
+        <div className=" flex gap-4">
+          <div className=" h-5 w-5 rounded-md bg-gray-500"></div>{" "}
+          <span>Contact</span>
+        </div>
+        <div className=" flex flex-col gap-5">
+        <h5 className=" text-white text-2xl">Stay in contact</h5>
+        <div className=" flex gap-4">
+          <div className=" h-5 w-5 rounded-md bg-gray-500"></div>{" "}
+          <span>info@contact.com</span>
+        </div>
+        <div className=" flex gap-4">
+          <div className=" h-5 w-5 rounded-md bg-gray-500"></div>{" "}
+          <span>+34 654 654 654</span>
+        </div>
       </div>
-      <div> sec 3</div>
+      </div>
+      <div className=" min-w-[450px] flex flex-col gap-5 items-center border-2 border-white text-white rounded-sm px-10 py-4"> 
+        <h5 className=" text-3xl text-center max-w-[300px]">Suscribe now to the 
+        newsletter</h5>
+        <button className=" py-5 border-2 border-white w-full rounded-md">Your Name</button>
+        <button className=" py-5 border-2 border-white w-full rounded-md">Your Email</button>
+        <div className=" flex gap-5 text-xl">
+            <input type="checkbox" name="" id="" />
+            <span>I consent the privacy policy</span>
+        </div>
+        <button className=" py-5 text-black font-bold bg-white w-full rounded-md">Subscribe!</button>
+      </div>
     </div>
   );
 }
