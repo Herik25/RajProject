3709ce998632361dfd63a714db6ac308982a68ed
Herik25 <128887958+Herik25@users.noreply.github.com>
2024-08-03 10:06:37 +0530

    taiwind init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>harsh parmar</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
